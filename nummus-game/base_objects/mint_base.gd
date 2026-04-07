9022c7ee3e33a43f9ed9d5f008097990cc626a06
3M Arya <131737618+SLJ900-BRIDGE-GIRDER@users.noreply.github.com>
2026-04-07 01:06:50 -0500

    feature: mints as resource and scene

--- nummus-game/base_objects/mint_base.gd
+++ nummus-game/base_objects/mint_base.gd
@@ -1,14 +1,18 @@
-@abstract
-class_name Mint extends Node3D
+extends Node3D
+
+class_name Mint 
 
 var period_length: int = -1
 var value
+
+@export var coin_id: MintStats
 
 func _init(given_value, given_period_length: int = -1):
 	value = given_value
 	period_length = given_period_length
 
-@abstract func run_effect()
+func run_effect():
+	pass
 
 func condition_met() -> bool: # coins without a condition will always run
 	return true
---
+++ nummus-game/resources/mints/mint_class.gd
@@ -0,0 +1,6 @@
+extends Resource
+class_name MintStats
+
+@export var json_id: String
+@export var mint_texture: Texture2D
+@export var effect: GDScript
